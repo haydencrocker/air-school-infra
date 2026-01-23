80c90d86b4a51d30d8488ba59019e883ce3dbbae
Hayden Crocker <hayden@gogreenish.org>
2026-01-22 17:12:48 -0800

    Update ingest_worker.py

--- src/ingest_worker.py
+++ src/ingest_worker.py
@@ -1,7 +1,7 @@
 import time
 
-print("ingest worker started")
+print("ingest worker started", flush=True)
 
 while True:
-    print("worker alive, sleeping...")
+    print("worker alive, sleeping...", flush=True)
     time.sleep(60)
